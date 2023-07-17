1396913424d6d96b6a8ab55ef7c9c3db6ad82f1a
Andrew Muhsen Paramedis <Andrew.naaem99@eng-st.cu.edu.eg>
2023-07-17 07:24:10 +0300

    Upload paper pdf

--- Pulmo-AI/fbapp.py
+++ Pulmo-AI/fbapp.py
@@ -7,17 +7,18 @@ ph = PasswordHasher()
 
 storage_loaction = "pulmoai-b3fa7.appspot.comg/images"
 
-config = {
-    "apiKey": "AIzaSyBMDEOOYHVWtPYuzC1eaYw_vU_XOsd4SYg",
-    "authDomain": "pulmoai-b3fa7.firebaseapp.com",
-    "projectId": "pulmoai-b3fa7",
-    "storageBucket": "pulmoai-b3fa7.appspot.com",
-    "messagingSenderId": "244590506149",
-    "appId": "1:244590506149:web:b05d76aed1127694225d4c",
-    "measurementId": "G-Q4C767572X",
-    # "serviceAccount": r"Creds\firestore-creds.json",
-    "databaseURL": "https://pulmoai-b3fa7-default-rtdb.europe-west1.firebasedatabase.app/",
-}
+
+
+# config = {
+#     "apiKey": "AIzaSyBMDEOOYHVWtPYuzC1eaYw_vU_XOsd4SYg",
+#     "authDomain": "pulmoai-b3fa7.firebaseapp.com",
+#     "projectId": "pulmoai-b3fa7",
+#     "storageBucket": "pulmoai-b3fa7.appspot.com",
+#     "messagingSenderId": "244590506149",
+#     "appId": "1:244590506149:web:b05d76aed1127694225d4c",
+#     "measurementId": "G-Q4C767572X",
+#     "databaseURL": "https://pulmoai-b3fa7-default-rtdb.europe-west1.firebasedatabase.app/",
+# }
 
 
 # * old method for firebase
---
+++ Pulmo-AI/readme.md
@@ -0,0 +1,9 @@
+# Application guidlines
+
+## 1. Connection to firebase
+- Create a firebase project
+- Create a service account
+- Download the service account json file
+
+
+## 2. Creating Conda environment
